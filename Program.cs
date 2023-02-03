63a4d8362c678a6696ee5a83c9f9e48bb450aa52
lind-exe <jojje_lind@live.se>
2023-02-03 13:23:28 +0100

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿namespace WeatherApp5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            Console.WriteLine("asdasdwefwe");
+        }
+    }
+}
