2c90415ec99d7d3ca37662d6da9b85128e35b30c
lind-exe <jojje_lind@live.se>
2023-02-06 14:54:26 +0100

    Menus

---
+++ Methods/Menus.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeatherApp5.Methods
+{
+    internal class Menus
+    {
+        enum Main
+        {
+            Average_Temp_Per_Day = 1,
+            Average_Temp_Sorted,
+            Humidity_Sorted_And_Average_Humidity,
+            Mold_Risk,
+            Meterologic_Dates
+        }
+        public static void Show(string value)
+        {
+            bool goMain = true;
+
+            if (value == "Main")
+            {
+                while (goMain)
+                {
+                    foreach (int i in Enum.GetValues(typeof(Main)))
+                    {
+                        Console.WriteLine($"{i}. {Enum.GetName(typeof(Main), i).Replace("_", " ")}");
+                    }
+
+                    int nr;
+                    Main menu = (Main)99; //Default
+                    if (int.TryParse(Console.ReadKey(true).KeyChar.ToString(), out nr) || nr > Enum.GetNames(typeof(Main)).Length - 1)
+                    {
+                        menu = (Main)nr;
+                        Console.Clear();
+                    }
+                    else
+                    {
+                        //Helpers.WrongInput();
+                    }
+                    switch (menu)
+                    {
+                        case Main.Average_Temp_Per_Day:
+                            View.AverageTempPerDay();
+
+                            goMain = false;
+                            break;
+                        case Main.Average_Temp_Sorted:
+                            View.AverageTempSorted();
+
+                            
+                            goMain = false;
+                            break;
+                        case Main.Humidity_Sorted_And_Average_Humidity:
+                            View.HumiditySortedAndAverageHumitidy();
+
+                            goMain = false;
+                            break;
+                        case Main.Mold_Risk:
+                            View.MoldRisk();
+
+                            goMain = false;
+                            break;
+                        case Main.Meterologic_Dates:
+                            View.MeterologicDates();
+
+                            goMain = false;
+                            break;
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Methods/View.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeatherApp5.Methods
+{
+    internal class View
+    {
+        internal static void AverageTempPerDay()
+        {
+            Console.WriteLine("Enter date you wish to view average temps for: ");
+            string input = Console.ReadLine();
+
+
+
+
+        }
+
+        internal static void AverageTempSorted()
+        {
+            throw new NotImplementedException();
+        }
+
+        internal static void HumiditySortedAndAverageHumitidy()
+        {
+            throw new NotImplementedException();
+        }
+
+        internal static void MeterologicDates()
+        {
+            throw new NotImplementedException();
+        }
+
+        internal static void MoldRisk()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
