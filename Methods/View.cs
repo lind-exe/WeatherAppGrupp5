83a365b14a04b1d7d5020c25a2bb3d1e2182f8d0
lind-exe <jojje_lind@live.se>
2023-02-07 12:27:38 +0100

    testing avrg data

--- Methods/View.cs
+++ Methods/View.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using WeatherApp5.Data;
 
@@ -19,7 +20,75 @@ namespace WeatherApp5.Methods
 
         internal static void AverageTempSorted()
         {
-            throw new NotImplementedException();
+            //Console.WriteLine("Average temps sorted from highest to lowest: ");
+
+            //List<double> list = new List<double>();
+            //using (StreamReader r = new StreamReader("../../../Data/tempdata5-med fel.txt"))
+            //{
+            //    string content = r.ReadToEnd();
+
+            //    foreach (Match m in Regex.Matches(content, "Inne,(-?\\s?\\d+.\\d)"))
+            //    {
+            //        foreach (Group g in m.Groups)
+            //        {
+            //            if (int.Parse(g.Name) == 1)
+            //            {
+            //                double temp = double.Parse(g.Value, System.Globalization.CultureInfo.InvariantCulture);
+            //                list.Add(temp);
+
+            //            }
+            //        }
+            //    }
+            //}
+            //list.Sort();
+            //list.Reverse();
+            //foreach (var item in list)
+            //{
+            //    Console.WriteLine(item);
+            //}
+            //Console.WriteLine(list.Count());
+
+            for (int i = 6; i <= 12; i++)
+            {
+                Console.WriteLine("\tMånad " + i);
+                if (i < 10)
+                {
+                    for (int x = 1; x < 31; x++)
+                    {
+                        if (x < 10)
+                        {
+                            Regex regex = new Regex($"2016-0{i}-0{x}");
+                            MatchCollection matches = regex.Matches(File.ReadAllText("../../../Data/tempdata5-med fel.txt"));
+                            Console.WriteLine("dag " + x + ": " + matches.Count);
+                        }
+                        else
+                        {
+                            Regex regex2 = new Regex($"2016-0{i}-{x}");
+                            MatchCollection matches = regex2.Matches(File.ReadAllText("../../../Data/tempdata5-med fel.txt"));
+                            Console.WriteLine("dag " + x + ": " + matches.Count);
+                        }
+                    };
+                }
+                else
+                {
+                    for (int x = 1; x < 31; x++)
+                    {
+                        if (x < 10)
+                        {
+                            Regex regex = new Regex($"2016-{i}-0{x}");
+                            MatchCollection matches = regex.Matches(File.ReadAllText("../../../Data/tempdata5-med fel.txt"));
+                            Console.WriteLine("dag " + x + ": " + matches.Count);
+                        }
+                        else
+                        {
+                            Regex regex2 = new Regex($"2016-{i}-{x}");
+                            MatchCollection matches = regex2.Matches(File.ReadAllText("../../../Data/tempdata5-med fel.txt"));
+                            Console.WriteLine("dag " + x + ": " + matches.Count);
+                        }
+                    };
+                }
+            }
+            Console.ReadLine();
         }
 
         internal static void HumiditySortedAndAverageHumitidy()
