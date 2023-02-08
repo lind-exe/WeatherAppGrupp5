2902a701ce4f174b008cabaf5c7ad5473df63fc3
Hypzter <cgranfeldt@hotmail.com>
2023-02-08 16:28:27 +0100

    Average temp calc'd

--- Methods/View.cs
+++ Methods/View.cs
@@ -9,6 +9,7 @@ using System.Text.RegularExpressions;
 using WeatherApp5.Data;
 using System.Runtime.CompilerServices;
 using System.Globalization;
+using System.Security.Cryptography.X509Certificates;
 
 namespace WeatherApp5.Methods
 {
@@ -18,22 +19,35 @@ namespace WeatherApp5.Methods
         internal static void AverageTempPerDay()
         {
             List<WeatherData> weatherData = RegexData.GetData();
-
+            
             Console.Write("Choose a day to check averagetemp (YYYY-MM-DD): ");
             string date = Console.ReadLine();
 
-            weatherData.Select(x => x.Date == DateTime.Parse(date)).Where(x => x.Equals(DateTime.Parse(date)));
-               
+            var chosenData = weatherData.Where(x => x.Date.Date == DateTime.Parse(date)).Where(x => x.Location == "Inne").ToList();
+            double counter = 0;
+
+            
+            foreach (var c in chosenData)
+            {
+                    Console.WriteLine(c.Date + " " + c.Location + " " + c.Temperature + " " + c.Humidity);
+                    counter += c.Temperature;
+            }
+            double result = counter / chosenData.Count;
+            Console.WriteLine();
+            Console.WriteLine("Antal data: " + chosenData.Count);
+            Console.WriteLine("Medeltemp: " + result);
+
+
         }
 
         internal static void AverageTempSorted()
         {
-            
+
         }
 
         internal static void HumiditySortedAndAverageHumitidy()
         {
-           
+
         }
         internal static void MeterologicDates()
         {
