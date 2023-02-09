d60f2a05d9d65bb7624079b642fbe98e3e539335
Hypzter <cgranfeldt@hotmail.com>
2023-02-09 13:43:41 +0100

    AveragetempSorted complete

--- Methods/View.cs
+++ Methods/View.cs
@@ -10,6 +10,8 @@ using WeatherApp5.Data;
 using System.Runtime.CompilerServices;
 using System.Globalization;
 using System.Security.Cryptography.X509Certificates;
+using System.Runtime.Intrinsics.Arm;
+using System.Reflection;
 
 namespace WeatherApp5.Methods
 {
@@ -45,7 +47,7 @@ namespace WeatherApp5.Methods
         internal static void AverageTempSorted()
         {
             List<WeatherData> weatherData = RegexData.GetData();
-            List<DataPerDay> listDataPerDay = new();
+            Dictionary<string, double> data = new Dictionary<string, double>();
 
             DateTime StartDate = new DateTime(2016, 6, 1);
             DateTime EndDate = new DateTime(2016, 12, 31);
@@ -54,27 +56,39 @@ namespace WeatherApp5.Methods
             Console.WriteLine("Inne eller Ute? ");
             string inOrOut = Console.ReadLine();
 
-            List<DateTime> dateList = new List<DateTime>();
             while (StartDate.AddDays(DayInterval) <= EndDate)
             {
-
                 var chosenData = weatherData.Where(x => x.Date.Date == StartDate).Where(x => x.Location == inOrOut).ToList();
                 double counter = 0;
-
 
                 foreach (var c in chosenData)
                 {
                     counter += c.Temperature;
                 }
                 double result = counter / chosenData.Count;
+
                 if (!double.IsNaN(result))
                 {
-                    Console.WriteLine(StartDate.ToString("yyyy-MM-dd") + "- medeltemp: " + Math.Round(result, 1) + "°");
-
+                    //Console.WriteLine(StartDate.ToString("yyyy-MM-dd") + " - Medeltemp: " + Math.Round(result, 1) + "°");
+                    data.Add(StartDate.ToString("yyyy-MM-dd"), Math.Round(result, 1));
                 }
 
                 StartDate = StartDate.AddDays(DayInterval);
             }
+            var topTenWarmest = data.OrderByDescending(x => x.Value).Take(10);
+            Console.WriteLine("Varmaste dagarna:");
+            foreach (var key in topTenWarmest)
+            {
+                Console.WriteLine(key.Key + " " + key.Value);
+            }
+            Console.WriteLine("--------------------");
+            Console.WriteLine("Kallaste dagarna:");
+            var topTenColdest = data.OrderBy(x => x.Value).Take(10);
+            foreach (var key in topTenColdest)
+            {
+                Console.WriteLine(key.Key + " " + key.Value);
+            }
+
         }
 
         internal static void HumiditySortedAndAverageHumitidy()
