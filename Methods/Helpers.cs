71033a7cc3c03542531015b014f83d3a3a728e70
lind-exe <jojje_lind@live.se>
2023-02-09 14:43:11 +0100

    some stuff

---
+++ Methods/Helpers.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeatherApp5.Methods
+{
+    internal class Helpers
+    {
+        public static int TryNumber(int maxValue, int minValue)
+        {
+            int number = 0;
+            bool correctInput = false;
+            while (!correctInput)
+            {
+                if (!int.TryParse(Console.ReadLine(), out number) || number > maxValue || number < minValue)
+                {
+                    Console.Write("Wrong input, try again: ");
+
+                }
+                else
+                {
+                    correctInput = true;
+                }
+            }
+            return number;
+        }
+        public static string SelectedLocation()
+        {
+            Console.WriteLine("1.Inne\n2.Ute ");
+            int inOrOut = Helpers.TryNumber(2, 1);
+            string input = "";
+            switch (inOrOut)
+            {
+                case 1:
+                    input = "Inne";
+                    break;
+                case 2:
+                    input = "Ute";
+                    break;
+            }
+            return input;
+        }
+    }
+}
--- Methods/View.cs
+++ Methods/View.cs
@@ -24,10 +24,10 @@ namespace WeatherApp5.Methods
 
             Console.Write("Välj en dag för att kolla medeltemp: (YYYY-MM-DD): ");
             string date = Console.ReadLine();
-            Console.Write("Inne eller ute: ");
-            string inOrOut = Console.ReadLine();
+            
+            string input = Helpers.SelectedLocation();
 
-            var chosenData = weatherData.Where(x => x.Date.Date == DateTime.Parse(date)).Where(x => x.Location == inOrOut).ToList();
+            var chosenData = weatherData.Where(x => x.Date.Date == DateTime.Parse(date)).Where(x => x.Location == input).ToList();
             double counter = 0;
 
 
@@ -53,12 +53,10 @@ namespace WeatherApp5.Methods
             DateTime EndDate = new DateTime(2016, 12, 31);
             int DayInterval = 1;
 
-            Console.WriteLine("Inne eller Ute? ");
-            string inOrOut = Console.ReadLine();
-
+            string input = Helpers.SelectedLocation();
             while (StartDate.AddDays(DayInterval) <= EndDate)
             {
-                var chosenData = weatherData.Where(x => x.Date.Date == StartDate).Where(x => x.Location == inOrOut).ToList();
+                var chosenData = weatherData.Where(x => x.Date.Date == StartDate).Where(x => x.Location == input).ToList();
                 double counter = 0;
 
                 foreach (var c in chosenData)
@@ -93,16 +91,118 @@ namespace WeatherApp5.Methods
 
         internal static void HumiditySortedAndAverageHumitidy()
         {
+            List<WeatherData> weatherData = RegexData.GetData();
+            Dictionary<string, double> data = new Dictionary<string, double>();
 
-        }
-        internal static void MeterologicDates()
-        {
-            throw new NotImplementedException();
+            DateTime StartDate = new DateTime(2016, 6, 1);
+            DateTime EndDate = new DateTime(2016, 12, 31);
+            int DayInterval = 1;
+
+            string input = Helpers.SelectedLocation();
+            while (StartDate.AddDays(DayInterval) <= EndDate)
+            {
+                var chosenData = weatherData.Where(x => x.Date.Date == StartDate).Where(x => x.Location == input).ToList();
+                double counter = 0;
+
+                foreach (var c in chosenData)
+                {
+                    counter += c.Humidity;
+                }
+                double result = counter / chosenData.Count;
+
+                if (!double.IsNaN(result))
+                {
+                    //Console.WriteLine(StartDate.ToString("yyyy-MM-dd") + " - Medeltemp: " + Math.Round(result, 1) + "°");
+                    data.Add(StartDate.ToString("yyyy-MM-dd"), Math.Round(result, 1));
+                }
+
+                StartDate = StartDate.AddDays(DayInterval);
+            }
+            var topTenWarmest = data.OrderByDescending(x => x.Value).Take(10);
+            Console.WriteLine("Fuktigaste dagarna:");
+            foreach (var key in topTenWarmest)
+            {
+                Console.WriteLine(key.Key + " " + key.Value);
+            }
+            Console.WriteLine("--------------------");
+            Console.WriteLine("Torraste dagarna:");
+            var topTenColdest = data.OrderBy(x => x.Value).Take(10);
+            foreach (var key in topTenColdest)
+            {
+                Console.WriteLine(key.Key + " " + key.Value);
+            }
+
         }
 
         internal static void MoldRisk()
         {
-            throw new NotImplementedException();
+            //List<WeatherData> weatherData = RegexData.GetData();
+
+            //DateTime StartDate = new DateTime(2016, 6, 1);
+            //DateTime EndDate = new DateTime(2016, 12, 31);
+            //int DayInterval = 1;
+
+            //string input = Helpers.SelectedLocation();
+            //while (StartDate.AddDays(DayInterval) <= EndDate)
+            //{
+            //    var chosenData = weatherData.Where(x => x.Date.Date == StartDate).Where(x => x.Location == input).ToList();
+            //    double result = 0;
+            //    double temp = 0;
+            //    double humidity = 0;
+            //    foreach (var c in chosenData)
+            //    {                    
+            //        temp += c.Temperature;
+            //        humidity += c.Humidity;
+            //    }
+            //    //double result = counter / chosenData.Count;
+
+            //    if (!double.IsNaN(result))
+            //    {
+            //        //Console.WriteLine(StartDate.ToString("yyyy-MM-dd") + " - Medeltemp: " + Math.Round(result, 1) + "°");
+            //        data.Add(StartDate.ToString("yyyy-MM-dd"), Math.Round(result, 1));
+            //    }
+
+            //    StartDate = StartDate.AddDays(DayInterval);
+            //}
+        }
+        internal static void MeterologicDates()
+        {
+            List<WeatherData> weatherData = RegexData.GetData();
+            Dictionary<string, double> data = new Dictionary<string, double>();
+
+            DateTime StartDate = new DateTime(2016, 8, 1);
+            DateTime EndDate = new DateTime(2016, 12, 31);
+            int DayInterval = 1;
+
+            
+            while (StartDate.AddDays(DayInterval) <= EndDate)
+            {
+                var chosenData = weatherData.Where(x => x.Date.Date == StartDate).Where(x => x.Location == "Ute").ToList();
+                double counter = 0;
+
+                foreach (var c in chosenData)
+                {
+                    counter += c.Temperature;
+                }
+                double result = counter / chosenData.Count;
+
+                if (!double.IsNaN(result))
+                {
+                    data.Add(StartDate.ToString("yyyy-MM-dd"), Math.Round(result, 1));
+                }
+
+                StartDate = StartDate.AddDays(DayInterval);
+            }
+
+
+            // 2016-10-04 är rätt datum för meterologisk höst
+
+            var sorted = data.OrderBy(x => x.Key);
+
+            foreach (var k in sorted)
+            {
+                Console.WriteLine(k.Key + " " + k.Value);
+            }
         }
     }
 }
